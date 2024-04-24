4806c5d3f7803a6db33a059506ab28ec721e2b7e
Pradnya R Ghadge <prghadge@csuchico.edu>
2024-04-23 22:14:56 -0700

    added db connection for signup post api

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import * as  clientController from './Controllers/clientsController.js'
 
 dotenv.config();
 import cors from 'cors';
-import bcrypt from 'bcrypt';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,31 +15,9 @@ app.get('/clients', clientController.getClients);
 app.use(cors());
 app.use(express.json()); // Parses incoming requests with JSON payloads
 
-// Dummy database connection
-// You need to replace this with your actual database connection logic
-const users = [];
+// Endpoint for /signup
+app.post('/signup', clientController.signup);
 
-app.post('/signup', async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-
-  // Here you should add input validation
-
-  try {
-    // Hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Replace the below logic with actual database insertion logic
-    const newUser = { firstName, lastName, email, password: hashedPassword };
-    users.push(newUser); // In a real app, you'd insert this into the database
-    console.log(users);
-    // Send back a successful response
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    console.error('Error during signup:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
 // Existing /api route
 app.get('/api', (req, res) => {
