72d84a8cd0f5170d09fcb09c0b912e8bc41d6fed
Pradnya R Ghadge <prghadge@csuchico.edu>
2024-04-30 10:05:34 -0700

    commit code

--- server/Controllers/clientsController.js
+++ server/Controllers/clientsController.js
@@ -55,43 +55,43 @@ const login = async (req, res) => {
 
 
 const signup = async (req, res) => {
-  // const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
-  // // Validate the input
-  // if (!firstName || !lastName || !email || !password) {
-  //   return res.status(400).json({ error: 'All fields are required' });
-  // }
+  // Validate the input
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
 
-  // try {
-  //   // Hash the password
-  //   const saltRounds = 10;
-  //   const hashedPassword = await bcrypt.hash(password, saltRounds);
+  try {
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  //   // Get database connection
-  //   const connection = await getConnection();
+    // Get database connection
+    const connection = await getConnection();
 
-  //   // Insert new user into the clients table
-  //   const query = `
-  //     INSERT INTO clients (fname, lname, email, role, is_assigned_trainer, password, created_at, updated_at)
-  //     VALUES (?, ?, ?, 'client', 0, ?, NOW(), NOW())
-  //   `;
+    // Insert new user into the clients table
+    const query = `
+      INSERT INTO clients (fname, lname, email, role, is_assigned_trainer, password, created_at, updated_at)
+      VALUES (?, ?, ?, 'client', 0, ?, NOW(), NOW())
+    `;
     
-  //   await new Promise((resolve, reject) => {
-  //     connection.query(query, [firstName, lastName, email, hashedPassword], (error, results, fields) => {
-  //       if (error) {
-  //         reject(error);
-  //       } else {
-  //         resolve(results);
-  //       }
-  //     });
-  //   });
+    await new Promise((resolve, reject) => {
+      connection.query(query, [firstName, lastName, email, hashedPassword], (error, results, fields) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
       
-  //     res.status(201).json({ message: 'User created successfully' });
+      res.status(201).json({ message: 'User created successfully' });
           
-  // } catch (err) {
-  //   console.error('Error during signup:', err);
-  //   res.status(500).json({ error: 'Error inserting user into the database' });
-  // }
+  } catch (err) {
+    console.error('Error during signup:', err);
+    res.status(500).json({ error: 'Error inserting user into the database' });
+  }
 };
 
 
